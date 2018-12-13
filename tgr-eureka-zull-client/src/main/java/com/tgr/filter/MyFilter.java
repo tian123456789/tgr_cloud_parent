8c6836ad0baa2f23500465300a36e1260587483e
zhuluwuqing <>
2018-12-13 15:28:29 +0800

    add zuul filter to filtering token

---
+++ tgr-eureka-zull-client/src/main/java/com/tgr/filter/MyFilter.java
@@ -0,0 +1,71 @@
+package com.tgr.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+
+@Component
+public class MyFilter extends ZuulFilter {
+
+	private static Log log = LogFactory.getLog(MyFilter.class);
+	
+	/* (non-Javadoc)
+	 * @see com.netflix.zuul.IZuulFilter#run()
+	 * 具体的过滤逻辑
+	 * 加过看看有没有token
+	 */
+	@Override
+	public Object run() {
+		RequestContext ctx = RequestContext.getCurrentContext();
+		HttpServletRequest request = ctx.getRequest();
+		Object accessToken = request.getParameter("token");
+		if(accessToken == null) {
+			log.warn("token is empty");
+			ctx.setSendZuulResponse(false);
+			ctx.setResponseStatusCode(401);
+			try {
+				ctx.getResponse().getWriter().write("token is empty");
+			} catch (Exception e) {
+				return null;
+			}
+		}
+		log.info("ok");
+		return null;
+		//http://127.0.0.1:5000/v1/hiapi/hi?name=opop		token is empty
+		//http://127.0.0.1:5000/v1/hiapi/hi?name=opop&token=123		hi opop , i am from port:8764
+	}
+
+	/* (non-Javadoc)
+	 * @see com.netflix.zuul.IZuulFilter#shouldFilter()
+	 * 表示该过滤器是否过滤逻辑 为true则执行run()方法
+	 */
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.netflix.zuul.ZuulFilter#filterOrder()
+	 * 过滤顺序 值为Int 值越小越早执行该过滤器
+	 */
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see com.netflix.zuul.ZuulFilter#filterType()
+	 * 过滤器类型
+	 * 四种: pre,post,routing,error
+	 */
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+}
