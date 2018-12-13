5a8ce8d28be5087beb8ea5ebb44f3c9d44ea783a
zhuluwuqing <>
2018-12-13 15:33:32 +0800

    plc

--- tgr-eureka-zull-client/src/main/java/com/tgr/filter/MyFilter.java
+++ tgr-eureka-zull-client/src/main/java/com/tgr/filter/MyFilter.java
@@ -9,6 +9,10 @@ import org.springframework.stereotype.Component;
 import com.netflix.zuul.ZuulFilter;
 import com.netflix.zuul.context.RequestContext;
 
+/**
+ * @author tgr
+ *注入到IOC赶紧去过滤token吧
+ */
 @Component
 public class MyFilter extends ZuulFilter {
 
@@ -38,6 +42,8 @@ public class MyFilter extends ZuulFilter {
 		return null;
 		//http://127.0.0.1:5000/v1/hiapi/hi?name=opop		token is empty
 		//http://127.0.0.1:5000/v1/hiapi/hi?name=opop&token=123		hi opop , i am from port:8764
+		//可见， MyFilter 这个 Bean 注入 IoC 容器之后，对请求进行了过滤，并在请求路由转发
+		//之前进行了逻辑判断。在实际开发中，可以用此过滤器进行安全验证
 	}
 
 	/* (non-Javadoc)
