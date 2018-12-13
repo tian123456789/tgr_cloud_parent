b30bd5e526885b70b978f27cf3003f2cd45a7d53
zhuluwuqing <>
2018-12-13 14:55:26 +0800

    zuul+hystrix

---
+++ tgr-eureka-zull-client/src/main/java/com/tgr/hystrix/fallback/MyFallbackProvider.java
@@ -0,0 +1,63 @@
+package com.tgr.hystrix.fallback;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.springframework.cloud.netflix.zuul.filters.route.ZuulFallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author tgr
+ * 给tgr-erueka-client服务加熔断
+ */
+@Component
+public class MyFallbackProvider implements ZuulFallbackProvider {
+
+	@Override
+	public String getRoute() {//如果需要所有的路由服务都加熔断功能,只需return "*";
+		return "tgr-cloud-client";
+	}
+
+	@Override
+	public ClientHttpResponse fallbackResponse() {
+		return new ClientHttpResponse() {
+			
+			@Override
+			public HttpHeaders getHeaders() {
+				HttpHeaders headers = new HttpHeaders();
+				headers.setContentType(MediaType.APPLICATION_JSON);
+				return headers;
+			}
+			
+			@Override
+			public InputStream getBody() throws IOException {
+				return new ByteArrayInputStream("oooops!errot, i'm the fallback.".getBytes());
+			}
+			
+			@Override
+			public String getStatusText() throws IOException {
+				return "OK";
+			}
+			
+			@Override
+			public HttpStatus getStatusCode() throws IOException {
+				return HttpStatus.OK;
+			}
+			
+			@Override
+			public int getRawStatusCode() throws IOException {
+				return 200;
+			}
+			
+			@Override
+			public void close() {
+			}
+		};
+	}
+
+}
